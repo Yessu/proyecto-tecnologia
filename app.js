f2c331484a756a75434c9d59a53f57fd6340f1f4
Yessi Bayo <31170051+Yessu@users.noreply.github.com>
2017-11-07 08:56:55 -0400

    app
    chat sockets

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+	app = 	  express(),
+	server =  require('http').Server(app),
+	io = 	  require('socket.io')(server),
+	users = {};
+
+	server.listen(3000);
+
+	app.get('/',function(req,res){
+		res.sendfile(__dirname + '/index.html')
+	});
+
+ io.sockets.on('connection',function(socket){
+	 socket.on('new user',function(data, callback) {
+ 		if (data in users) {
+ 			callback(false);
+ 		}else{
+ 			callback(true);
+ 			socket.nickname = data;
+ 			users[socket.nickname]=socket;
+ 			updateNicknames();
+ 		}
+ 	});
+	 
+ 	function updateNicknames(){
+ 		io.sockets.emit('usernames',Object.keys(users));
+ 	}
+ 	socket.on('send message', function(data){
+ 		io.sockets.emit('new message',{msg: data,nick:socket.nickname});
+ 	});
+
+ 	socket.on('disconnect',function(data){
+ 		if (!socket.nickname)return;
+ 		delete users[socket.nickname];
+ 		updateNicknames();
+ 	});
+});
